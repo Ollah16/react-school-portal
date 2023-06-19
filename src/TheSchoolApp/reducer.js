57f9b7d7b38a0c7b3b14cfb58e623cc32cb77faa
Ollah16 <olaoluwaboluwatife@yahoo.com>
2023-06-19 09:49:57 -0400

    schoolAppUpdate

--- src/TheSchoolApp/reducer.js
+++ src/TheSchoolApp/reducer.js
@@ -125,7 +125,6 @@ const myReducer = (state, action) => {
             ...state,
             questionsArray: b.allQuest
         }
-        console.log(a)
         return a
     }
     if (action.type === "DELETE_TEST") {
